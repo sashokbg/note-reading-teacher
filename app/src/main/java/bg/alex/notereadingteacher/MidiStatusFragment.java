09e98ea275af5d0d48bf7be05e68185f1f20e7b4
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2019-10-03 23:17:11 +0200

    Added transition on midi status fragment

--- app/src/main/java/bg/alex/notereadingteacher/MidiStatusFragment.java
+++ app/src/main/java/bg/alex/notereadingteacher/MidiStatusFragment.java
@@ -5,6 +5,12 @@ import android.app.Fragment;
 import android.content.Context;
 import android.media.midi.MidiManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.transition.Fade;
+import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +18,12 @@ import android.view.ViewGroup;
 import bg.alex.notereadingteacher.midi.MidiAware;
 import bg.alex.notereadingteacher.midi.MidiHandler;
 
-public class MidiStatusFragment extends Fragment {
+public class MidiStatusFragment extends Fragment implements View.OnClickListener {
 
     private MidiHandler midiHandler;
+    private View view;
+    private ViewGroup fragment;
+    private boolean clicked = true;
 
     @Override
     public void onDestroyView() {
@@ -30,7 +39,9 @@ public class MidiStatusFragment extends Fragment {
         Activity activity = this.getActivity();
         MidiManager midiManager = (MidiManager) activity.getSystemService(Context.MIDI_SERVICE);
 
-        View view = inflater.inflate(R.layout.midi_status_fragment, container, false);
+
+        view = inflater.inflate(R.layout.midi_status_fragment, container, false);
+        view.setOnClickListener(this);
 
         if (!(activity instanceof MidiAware)) {
             midiHandler = new MidiHandler(null, midiManager, view);
@@ -45,8 +56,31 @@ public class MidiStatusFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        fragment = (ViewGroup ) view.findViewById(R.id.midi_status_fragment);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
     public void onStart() {
         midiHandler.openConnectedDevice();
         super.onStart();
+    }
+
+    @Override
+    public void onClick(View view) {
+        TransitionManager.beginDelayedTransition(fragment);
+
+        if(clicked) {
+            ConstraintLayout.LayoutParams originalLayoutParams = (ConstraintLayout.LayoutParams) fragment.getLayoutParams();
+            originalLayoutParams.goneTopMargin = 60;
+            fragment.setLayoutParams(originalLayoutParams);
+        } else {
+            ConstraintLayout.LayoutParams originalLayoutParams = (ConstraintLayout.LayoutParams) fragment.getLayoutParams();
+            originalLayoutParams.goneTopMargin = 0;
+            fragment.setLayoutParams(originalLayoutParams);
+        }
+
+        clicked = !clicked;
     }
 }
